d5f5b777379b9cdae34c0b5fa722df7a3ec944a0
Jane Kim <jane.kim0717@gmail.com>
2022-05-16 13:04:43 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+import {HomePage} from './components/pages/HomePage';
+import Projects from './components/pages/Projects';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Blog from './components/pages/Blog'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path='/' exact component={HomePage}/>
+        <Route path='/projects' exact component={Projects} />
+        <Route path='/blog' exact component={Blog} />
+        <Route path='/about' exact component={About} />
+        <Route path='/contact' exact component={Contact} />
+      </Switch>
+
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaInstagram } from 'react-icons/fa';
+import { FiYoutube, FiTwitter } from 'react-icons/fi'
+import { mobile } from '../responsive'
+import { NavLink as Link } from 'react-router-dom';
+
+
+const LogoImg = styled.img`
+  width: 100px;
+  height: 80px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`; 
+
+const Container = styled.div`
+  height: 90px;
+  background-color: black;
+  ${mobile({ height: "50px" })}
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  color: white;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  color: white;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
+const MenuItem = styled.div`
+  font-size: 15px;
+  cursor: pointer;
+  margin-left: 25px;
+  color: white;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
+
+const SocialItem = styled.div`
+  font-size: 20px;
+  cursor: pointer;
+  margin-right: 25px;
+  color: white;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+  &.active {
+    color: #15cdfc;
+  }
+`;
+
+
+
+
+const NavBar = () => {
+  return (
+
+    <Container>
+      <Wrapper>
+        <Left>
+          <SocialItem><a href='https://www.instagram.com/stevefamine/'><FaInstagram /></a></SocialItem> 
+          <SocialItem><a href='https://www.youtube.com/stevefamine'><FiYoutube /></a></SocialItem> 
+          <SocialItem><a href='https://mobile.twitter.com/stevefamine'><FiTwitter /></a></SocialItem>
+        </Left>
+        <Center>
+          <Logo><LogoImg src={require('../assets/SFlogo_white.png')} alt='logo' /></Logo>
+        </Center>
+        <Right>
+          <NavLink to='/' activeStyle> <MenuItem>HOME</MenuItem> </NavLink>
+          <NavLink to='/projects' activeStyle><MenuItem>PROJECTS</MenuItem></NavLink>
+          <NavLink to='/blog' activeStyle><MenuItem>BLOG</MenuItem></NavLink>
+          <NavLink to='/about' activeStyle><MenuItem>ABOUT</MenuItem></NavLink>
+          <NavLink to='/contact' activeStyle><MenuItem>CONTACT</MenuItem></NavLink>
+        </Right>
+      </Wrapper>
+    </Container>
+    // <>
+    //   <Nav>
+    //     <NavLink to='/'>
+    //       <LogoImg src={require('../assets/SFlogo_white.png')} alt='logo' />
+    //     </NavLink>
+    //     <Bars /> 
+    //     <NavMenu>
+    //       <NavLink to='/' activeStyle>
+    //         HOME
+    //       </NavLink>
+    //       <NavLink to='/projects' activeStyle>
+    //         PROJECTS
+    //       </NavLink>
+    //       <NavLink to='/blog' activeStyle>
+    //         BLOG
+    //       </NavLink>
+    //       <NavLink to='/about' activeStyle>
+    //         ABOUT
+    //       </NavLink>
+    //       <NavLink to='/contact' activeStyle>
+    //         CONTACT
+    //       </NavLink>
+    //     </NavMenu>
+    //   </Nav>
+    // </>
+  )
+}
+
+export default NavBar
